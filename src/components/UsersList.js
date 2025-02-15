445f2ad8d3c62fbd6856e51801c795edbe24dd18
Dimitris Stavroulakis <jimstav@gmail.com>
2025-02-15 18:49:02 +0200

    Better Skeleton Display

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -20,27 +20,27 @@ function UsersList() {
     doCreateUser();
   };
 
+  let content;
+
   if (isLoadingUsers) {
-    return <Skeleton times={6} className="h-10 w-full" />;
-  }
-
-  if (loadingUsersError) {
-    return <div>Error fetching data: {loadingUsersError.message}</div>;
-  }
-
-  const renderedUsers = data.map((user) => {
-    return (
-      <div key={user.id} className="mb-2 border rounded">
-        <div className="flex p-2 justify-between items-center cursor-pointer">
-          {user.name}
+    content = <Skeleton times={6} className="h-10 w-full" />;
+  } else if (loadingUsersError) {
+    content = <div>Error fetching data: {loadingUsersError.message}</div>;
+  } else {
+    content = data.map((user) => {
+      return (
+        <div key={user.id} className="mb-2 border rounded">
+          <div className="flex p-2 justify-between items-center cursor-pointer">
+            {user.name}
+          </div>
         </div>
-      </div>
-    );
-  });
+      );
+    });
+  }
 
   return (
     <div>
-      <div className="flex flex-row justify-between m-3">
+      <div className="flex flex-row justify-between items-center m-3">
         <h1 className="m-2 text-xl">Users</h1>
         <Button loading={isCreatingUser} onClick={handleUserAdd}>
           + Add User
@@ -48,7 +48,7 @@ function UsersList() {
         {creatingUserError &&
           `Error creating user: ${creatingUserError.message}`}
       </div>
-      {renderedUsers}
+      {content}
     </div>
   );
 }
