d8be8b0323bbde2d62134ba66aa63ff2e37d245d
Dimitris Stavroulakis <jimstav@gmail.com>
2025-02-16 16:56:37 +0200

    Fixing a Delete Error

--- src/store/slices/usersSlice.js
+++ src/store/slices/usersSlice.js
@@ -39,9 +39,7 @@ const usersSlice = createSlice({
     });
     builder.addCase(removeUser.fulfilled, (state, action) => {
       state.isLoading = false;
-      // TODO: FIX ME
-      console.log(action);
-      // state.data.filter((user) => user.id !== action.payload.id);
+      state.data = state.data.filter((user) => user.id !== action.payload.id);
     });
     builder.addCase(removeUser.rejected, (state, action) => {
       state.isLoading = false;
