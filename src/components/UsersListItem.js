637850db9e9681c407b587d81a3997ffd0fae766
Dimitris Stavroulakis <jimstav@gmail.com>
2025-02-16 16:45:06 +0200

    Deleting the User

--- src/components/UsersListItem.js
+++ src/components/UsersListItem.js
@@ -1,7 +1,22 @@
+import { GoTrashcan } from "react-icons/go";
+import Button from "./Button";
+import { useThunk } from "../hooks/useThunk";
+import { removeUser } from "../store";
+
 function UsersListItem({ user }) {
+  const [doRemoveUser, isLoading, error] = useThunk(removeUser);
+
+  const handleClick = () => {
+    doRemoveUser(user);
+  };
+
   return (
     <div className="mb-2 border rounded">
       <div className="flex p-2 justify-between items-center cursor-pointer">
+        <Button loading={isLoading} onClick={handleClick}>
+          <GoTrashcan />
+        </Button>
+        {error && <div>Error deleting user: {error.message}</div>}
         {user.name}
       </div>
     </div>
