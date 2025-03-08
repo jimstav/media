8ea813c7b69c8f789ad63720938185ee040d0584
Dimitris Stavroulakis <jimstav@gmail.com>
2025-03-08 18:40:48 +0200

    Showing the List of Photos

--- src/components/PhotosListItem.js
+++ src/components/PhotosListItem.js
@@ -1,5 +1,9 @@
-function PhotosListItem() {
-  return <div>PhotosListItem</div>;
+function PhotosListItem({ photo }) {
+  return (
+    <div>
+      <img src={photo.url} alt="random pic" className="h-20 w-20" />
+    </div>
+  );
 }
 
 export default PhotosListItem;
